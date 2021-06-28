d5fd8645a2f3afefc74ab014faef41efddb233c8
Adrián Avilés <adrian8167e@gmail.com>
2021-06-28 04:16:24 -0600

    implementar caracteristica de registro de comentarios para rh

---
+++ components/contacto/RegistroMensajeContactoDialog.vue
@@ -0,0 +1,146 @@
+<template>
+  <v-dialog :value="value" persistent max-width="600px">
+    <v-form ref="form" v-model="valid" lazy-validation @submit.prevent="crearMensajeContacto">
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">
+            <v-icon left>fas fa-comment</v-icon>
+            Contacto
+          </span>
+        </v-card-title>
+
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col>
+                <v-text-field
+                  label="Nombre"
+                  outlined
+                  v-model="creacionMensajeContacto.command.nombre"
+                  :disabled="creacionMensajeContacto.isExecuting"
+                  :rules="rules.nombre"
+                ></v-text-field>
+
+                <v-text-field
+                  label="Correo"
+                  type="email"
+                  outlined
+                  v-model="creacionMensajeContacto.command.correo"
+                  :disabled="creacionMensajeContacto.isExecuting"
+                  :rules="rules.correo"
+                ></v-text-field>
+
+                <v-textarea
+                  label="Comentario"
+                  outlined
+                  v-model="creacionMensajeContacto.command.comentario"
+                  :disabled="creacionMensajeContacto.isExecuting"
+                  :rules="rules.comentario"
+                ></v-textarea>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn type="submit" color="primary" :disabled="creacionMensajeContacto.isExecuting">
+            <v-icon v-if="creacionMensajeContacto.isExecuting" left>fas fa-circle-notch fa-spin</v-icon>
+            <v-icon v-else left>fas fa-paper-plane</v-icon>
+            Enviar
+          </v-btn>
+          <v-btn text @click="close" :disabled="creacionMensajeContacto.isExecuting">
+            <v-icon left>fas fa-undo</v-icon>
+            Cancelar
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-form>
+
+    <v-snackbar v-if="creacionMensajeContacto.output" :value="true" color="success">
+      Tu comentario se ha registrado con éxito.
+    </v-snackbar>
+
+    <v-snackbar v-if="creacionMensajeContacto.error" :value="true" color="error">
+      {{ creacionMensajeContacto.error.message || creacionMensajeContacto.error }}
+    </v-snackbar>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'RegistroMensajeContactoDialog',
+  props: {
+    value: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    const nombreMaxLength = 50;
+    const comentarioMaxLength = 100;
+
+    return {
+      valid: true,
+      rules: {
+        nombre: [
+          v => !!v || 'El nombre es requerido',
+          v => (v && v.length <= nombreMaxLength) || `El nombre excede los ${nombreMaxLength} caracteres permitidos`,
+        ],
+        correo: [
+          v => !!v || 'El correo es requerido',
+          v => /.+@.+\..+/.test(v) || 'La dirección de correo no es válida',
+        ],
+        comentario: [
+          v => !!v || 'El comentario es requerido',
+          v => (v && v.length <= comentarioMaxLength) || `El comentario excede los ${comentarioMaxLength} caracteres permitidos`,
+        ]
+      },
+      nombreMaxLength,
+      comentarioMaxLength,
+      creacionMensajeContacto: {
+        command: {
+          nombre: null,
+          correo: null,
+          comentario: null
+        },
+        isExecuting: false
+      }
+    };
+  },
+  methods: {
+    async crearMensajeContacto() {
+      if (this.creacionMensajeContacto.isExecuting) {
+        return;
+      }
+
+      if (!this.$refs.form.validate()) {
+        return;
+      }
+
+      const command = {
+        ...this.creacionMensajeContacto.command
+      };
+
+      this.creacionMensajeContacto.isExecuting = true;
+      try {
+        const { result } = await this.$axios.$post('http://localhost:8080/mensaje-contacto', command);
+        this.$emit('success', result);
+        this.close();
+      } catch (error) {
+        this.$emit('error', error);
+      } finally {
+        this.creacionMensajeContacto.isExecuting = false;
+      }
+    },
+    close() {
+      this.$refs.form.reset();
+      this.$emit('input', false);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -5,17 +5,54 @@
         <v-icon left>fas fa-bullhorn</v-icon>
         <span>{{ this.title }}</span>
       </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon @click="mensajeContactoDialog = true">
+        <v-icon>far fa-envelope</v-icon>
+      </v-btn>
     </v-app-bar>
     <v-main>
       <RouterView />
     </v-main>
+
+    <registro-mensaje-contacto-dialog
+      v-model="mensajeContactoDialog"
+      @success="mensajeContactoExitoso"
+      @error="mensajeContactoFallido"
+    />
+
+    <v-snackbar v-if="resultadoMensajeContacto.exito" :value="true" color="success">
+      Tu comentario se ha registrado con éxito.
+    </v-snackbar>
+    <v-snackbar v-if="resultadoMensajeContacto.error" :value="true" color="error">
+      {{ resultadoMensajeContacto.error.message || resultadoMensajeContacto.error }}
+    </v-snackbar>
   </v-app>
 </template>
 
 <script>
+import RegistroMensajeContactoDialog from '~/components/contacto/RegistroMensajeContactoDialog';
+
 export default {
+  components: {
+    RegistroMensajeContactoDialog
+  },
   data: () => ({
-    title: 'Cambalache'
-  })
+    title: 'Cambalache',
+    mensajeContactoDialog: false,
+    resultadoMensajeContacto: {
+      exito: false,
+      error: null
+    }
+  }),
+  methods: {
+    mensajeContactoExitoso() {
+      this.resultadoMensajeContacto.exito = true;
+    },
+    mensajeContactoFallido(error) {
+      this.resultadoMensajeContacto.error = error;
+    }
+  }
 }
 </script>
